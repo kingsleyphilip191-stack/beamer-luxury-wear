eda9bf938518aaea23c0595774668c55e2b2229a
kingsleyphilip191-stack <kingsleyphilip191@gmail.com>
2026-09-11 14:55:11 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('blw-v1').then(cache => cache.addAll(['./']))
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
